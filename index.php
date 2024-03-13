2b82c85271f021eab399959e2249b392eb1e8b0b
Leka610 <150241118+Leka610@users.noreply.github.com>
2024-03-14 01:47:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=egde">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div>
+        <?php include('sidebar.html');?>
+    </div>
+    <div>
+        <header>
+            <?php include('header.html'); ?>
+        </header>
+    </div>
+
+    <div class="center-content right-aligned">
+        <h1>Welcome to L&D Clothing</h1>
+        <p>Check out our New Arrivals!</p>
+        <a href="shop.php" class="shop-now-button">Shop Now</a>
+    </div>
+</body>
+</html>
