034262a7457fed55f49aa63ca78a94d5945feb67
dedinb3 <152423133+dedinb3@users.noreply.github.com>
2024-03-15 18:10:30 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    $fullname = $_POST['fullname'];
+    $gender = $_POST['gender'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    // Confirm password
+    if($password !== $confirm_password) {
+        $_SESSION['password_mismatch'] = true;
+        header('Location: signup.php');
+        exit();
+    }
+
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+    // Database connection
+    $conn = new mysqli('localhost', 'root', '', 'final_web');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    } else {
+
+        $stmt = $conn->prepare("INSERT INTO signup (fullname, username, gender, email, password, phone) VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param("sssssi", $fullname, $username, $gender, $email, $password_hash, $phone);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+
+        $_SESSION['signup_success'] = true;
+        header("Location: index.php");
+        exit();
+    }
+?>
